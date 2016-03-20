e01b181c05df5b824475e63b210e7517d617eac8
Vitaliy <vbykovec@gmail.com>
2016-03-20 21:42:38 +0200

    remove unused migration

--- db/migrate/001_create_user_auth_types.rb
+++
@@ -1,8 +0,0 @@
-class CreateUserAuthTypes < ActiveRecord::Migration
-  def change
-    create_table :user_auth_types do |t|
-      t.references :user, null: false
-      t.boolean :onelogin_auth, null: false, default: false
-    end
-  end
-end
